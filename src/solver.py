18adc9783c8cd1e42ad724a605d2f839bd665d6e
chengbin <sbkim0407@gmail.com>
2019-06-06 21:51:25 +0900

    add license information

--- src/solver.py
+++ src/solver.py
@@ -1,3 +1,9 @@
+# ---------------------------------------------------------
+# Tensorflow U-Net Implementation
+# Licensed under The MIT License [see LICENSE for details]
+# Written by Cheng-Bin Jin
+# Email: sbkim0407@gmail.com
+# ---------------------------------------------------------
 import os
 import cv2
 import numpy as np
